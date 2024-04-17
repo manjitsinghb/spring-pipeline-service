b9cd36b8d072000408a3a858d772e62c355830b3
manjit <manjitsingh.bits@gmail.com>
2024-04-17 00:26:31 -0700

    Add spring libraries - working pub sub

--- src/main/java/com/pipeline/configuration/PipelineConfiguration.java
+++ src/main/java/com/pipeline/configuration/PipelineConfiguration.java
@@ -1,12 +1,9 @@
 package com.pipeline.configuration;
 
-import com.pipeline.engine.core.configuration.Definition;
 import com.pipeline.engine.core.configuration.PipelineDefinition;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class PipelineConfiguration {
